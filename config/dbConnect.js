ed68f8439584306e0eff20f4da18f64e6878022d
hadi <hadibohluli1993@gmail.com>
2023-01-18 21:59:44 +0330

    late

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -12,19 +12,19 @@ const db = mysql.createConnection({
 db.connect(function(err) {
     if (err) throw err
     console.log("Connected!");
-    var sql3 = "CREATE TABLE IF NOT EXISTS users (userId VARCHAR(767), username VARCHAR(767), password VARCHAR(767) , bio VARCHAR(767) ,img VARCHAR(767) )";
+    var sql3 = "CREATE TABLE IF NOT EXISTS users (userId VARCHAR(128), username VARCHAR(128) UNIQUE, password VARCHAR(128) , bio VARCHAR(128) ,profile VARCHAR(128) )";
     db.query(sql3, function (err, result) {
       if (err) throw err;
     });
-    var sql = "CREATE TABLE IF NOT EXISTS posts (postId INTEGER(11), userId VARCHAR(767), caption VARCHAR(767) ,img VARCHAR(767) , date VARCHAR(767) , PRIMARY KEY (postId) )";
+    var sql = "CREATE TABLE IF NOT EXISTS posts (postId INTEGER(11), userId VARCHAR(128), caption VARCHAR(128) ,img VARCHAR(128) , date VARCHAR(128) , PRIMARY KEY (postId) )";
     db.query(sql, function (err, result) {
       if (err) throw err;
     }); 
-    var sql1 = "CREATE TABLE IF NOT EXISTS comments (commentId INTEGER(11), postId VARCHAR(767), userId VARCHAR(767) ,date VARCHAR(767) ,PRIMARY KEY (commentId))";
+    var sql1 = "CREATE TABLE IF NOT EXISTS comments (commentId INTEGER(11), postId VARCHAR(128), userId VARCHAR(128) ,date VARCHAR(128) ,PRIMARY KEY (commentId))";
     db.query(sql1, function (err, result) {
       if (err) throw err;
     }); 
-    var sql2 = "CREATE TABLE IF NOT EXISTS likes (id INTEGER(11), userId VARCHAR(767), postId VARCHAR(767) ,PRIMARY KEY (id))";
+    var sql2 = "CREATE TABLE IF NOT EXISTS likes (id INTEGER(11), userId VARCHAR(128), postId VARCHAR(128) ,PRIMARY KEY (id))";
     db.query(sql2, function (err, result) {
       if (err) throw err;
     }); 
